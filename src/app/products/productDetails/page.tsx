a60d0895757ad08736685418cc5a105e3e8bc1fe
Cgunter1 <cgunter@ucsc.edu>
2023-09-03 14:59:55 -0700

    migrate to pages

--- src/app/products/productDetails/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
-export default function Products() {
-    const searchParams = useSearchParams();
-    const product_num = searchParams?.get("productId");
-    return <h1>Product Details of {product_num}</h1>;
-}
